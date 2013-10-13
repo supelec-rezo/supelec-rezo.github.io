eddde4af891d53415f2afdc7e96179ffbbb888e9
Altay <altay@rez-gif.supelec.fr>
2013-10-13 22:35:19 +0200

    Ajout de la promo 2015

---
+++ promos/2015.php
@@ -0,0 +1,84 @@
+<div id="promo2014" class="promo">
+  <h2 class='promo-title'>Promotion 2015</h2>
+  
+  <div class="promo-left">
+    <h3>Bureau</h3>
+    
+    <table class="members">
+      <tr>
+        <th>Nicolas AUDEBERT</th>
+        <td>Président</td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Rémi ROBERT</th>
+        <td>Trésorier</td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Michel TORELLI</th>
+        <td>Secrétaire</td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Florian LEMAITRE</th>
+        <td>Vice-Président</td>
+        <td></td>
+      </tr>
+    </table>
+      
+    <h3>Conseil d'administration</h3>
+    
+    <table class="members">
+      <tr>
+        <th>Georges BADRO</th>
+        <td></td>
+		<td></td>
+      </tr>
+      <tr>
+        <th>Guillaume BORRITS</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Guillaume DHAINAUT</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Benoît FAURÉ</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Sélim MENOUAR</th>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+    
+    <h3>Autres membres</h3>
+    
+    <table class="members">
+      <tr>
+        <th>Jolan BONATO</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Francis DUPÉ</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Quentin PARIS</th>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+  </div>
+  
+  <div class="promo-right">
+    <img src="assets/images/rezomen/2015.jpg" alt="2015"/>
+  </div>
+</div>
