2a3c6434c8e15a20a4fae34b290b75405c953bca
{othian <othian@larez.fr>
2014-05-28 21:36:37 +0200

    Ajout de la promotion 2016

---
+++ promos/2016.php
@@ -0,0 +1,84 @@
+<div id="promo2016" class="promo">
+  <h2 class='promo-title'>Promotion 2016</h2>
+  
+  <div class="promo-left">
+    <h3>Bureau</h3>
+    
+    <table class="members">
+      <tr>
+        <th>Gauthier SEBAUX</th>
+        <td>Président</td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Julien LEPILLER</th>
+        <td>Trésorier</td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Jean-Sébastien RENAUD</th>
+        <td>Secrétaire</td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Julien MURGEY</th>
+        <td>Vice-Président</td>
+        <td></td>
+      </tr>
+    </table>
+      
+    <h3>Conseil d'administration</h3>
+    
+    <table class="members">
+      <tr>
+        <th>Fabien BERQUEZ</th>
+        <td></td>
+		<td></td>
+      </tr>
+      <tr>
+        <th>Hélène BOUDON</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Gabriel DEPARIS</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Paul MARGHERITTA</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Antoine RABANY</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Arthur VILLARD</th>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+    
+    <h3>Autres membres</h3>
+    
+    <table class="members">
+      <tr>
+        <th>Antoine BEAUGENDRE</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Renaud GUYOT</th>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+  </div>
+  
+  <div class="promo-right">
+    <img src="assets/images/rezomen/2015.jpg" alt="2015"/>
+  </div>
+</div>
