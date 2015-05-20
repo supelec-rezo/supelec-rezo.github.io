cdc4c72543165ac513c40e45b3e70faaeccfb4de
zorkroz <zorkroz@rez-gif.supelec.fr>
2015-05-20 20:28:37 +0200

    rajout de la promo 2017

---
+++ promos/2017.php
@@ -0,0 +1,99 @@
+<div id="promo2016" class="promo">
+  <h2 class='promo-title'>Promotion 2017</h2>
+  
+  <div class="promo-left">
+    <h3>Bureau</h3>
+    
+    <table class="members">
+      <tr>
+	  	<th>Eugène NELOU</th>
+		<td>Président</td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Louise MORIN</th>
+        <td>Trésorier</td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Adrien ROL</th>
+        <td>Secrétaire</td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Marc BARUSSAUD</th>
+        <td>Vice-Président</td>
+        <td></td>
+      </tr>
+    </table>
+      
+    <h3>Conseil d'administration</h3>
+    
+    <table class="members">
+      <tr>
+        <th>Benoît CASTEIGTS</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Héloïse DE LE BROUE DE VAREILLES-SOMMIERES</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Julian FARELLA-CHAMPEIX</th>
+        <td></td>
+        <td>(démissionnaire en avril 2015)</td>
+      </tr>
+      <tr>
+        <th>Ghislain FOURNIER</th>
+        <td></td>
+		<td></td>
+      </tr>
+      <tr>
+        <th>Nicolas GONCALVES</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+		<th>Valentin LE TOURNEUX DE LA PERRAUDIERE</th>
+		<td></td>
+        <td></td>
+	  </tr>
+      <tr>
+        <th>Yohann SILLAM</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Arnaud VENTURI</th>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+    
+    <h3>Autres membres</h3>
+    
+    <table class="members">
+      <tr>
+        <th>Jean-Baptiste DE PLINVAL</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Adrien PALAYRET</th>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Antoine REBEYRE</th>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+  </div>
+  
+  <div class="promo-right">
+    <img src="assets/images/rezomen/2017.jpg" alt="2017"/>
+  </div>
+</div>
