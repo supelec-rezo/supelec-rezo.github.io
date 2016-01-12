94460d845d53da0a4e6642bda473d3a36e5dae78
Marc Barussaud <marc.barussaud@rez-gif.supelec.fr>
2016-01-12 12:05:46 +0100

    utf-8

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <div id="footer">
   <div id="footer-content">
-    Page professionnelle de Supélec Rézo. <a href="contact/">Nous contacter</a>.</br>
-	&copy;<?php echo date("Y"); ?> Supélec Rézo. tous droits r�serv�s. 
+    Page professionnelle de Sup&eacute;lec R&eacute;zo. <a href="contact/">Nous contacter</a>.</br>
+	&copy;<?php echo date("Y"); ?> Sup&eacute;lec R&eacute;zo. tous droits r&eacute;serv&eacute;s. 
 
   </div>
 </div>
