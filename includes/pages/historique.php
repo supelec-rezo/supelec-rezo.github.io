e5f4c5c0cf6336e2beb3c6bedec56ebce06543f4
Marc Barussaud <marc.barussaud@rez-gif.supelec.fr>
2016-01-14 02:54:20 +0100

    Typo + màj robots.txt

--- includes/pages/historique.php
+++ includes/pages/historique.php
@@ -2,7 +2,7 @@
 
 <h3>2015</h3>
 <p>
-  Supélec Rézo fonde l'<a href="https://myares.fr">ARES</abbr> avec VIA et CR@NS, les associations réseau officiant respectivement sur le campus de CentraleSupélec à Châtenay-Malabry et celui de l'École Normale Supérieure à Chacan. Cette association sert d'interlocuteur aux instances responsables du Projet Paris-Saclay dans la promotion d'un réseau étudiant sur le campus de l'Université Paris-Saclay.
+  Supélec Rézo fonde l'<a href="https://myares.fr">ARES</a> avec VIA et CR@NS, les associations réseau officiant respectivement sur le campus de CentraleSupélec à Châtenay-Malabry et celui de l'École Normale Supérieure à Chacan. Cette association sert d'interlocuteur aux instances responsables du Projet Paris-Saclay dans la promotion d'un réseau étudiant sur le campus de l'Université Paris-Saclay.
   </br></br>
   Supélec Rézo a aussi proocédé à l'amélioration de sa fibre, passant d'un débit de 100Mb/s à 1Gb/s grâce à la coopération avec la DSI de l'Université Paris-Sud.
  </p>
