1869f8009d8f66b8505699d677eca70a28ef84f3
Julien Lepiller <julien@lepiller.eu>
2016-02-20 14:36:37 +0100

    pas de lien absolu

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,8 @@
 <div id="banner">
   <div id="banner-content">
 	<div id="flags">
-		<a href="?locale=0" title="français"><img src="/assets/images/fr.png" /></a>
-		<a href="?locale=1" title="english"><img src="/assets/images/en.png" /></a>
+		<a href="?locale=0" title="français"><img src="assets/images/fr.png" /></a>
+		<a href="?locale=1" title="english"><img src="assets/images/en.png" /></a>
 	</div>
   </div>
 </div>
