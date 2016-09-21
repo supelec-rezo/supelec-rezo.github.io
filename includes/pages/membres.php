9ab94894ef9315e8a39c1197844773fc3b1aa9b3
Remi Rampin <remirampin@gmail.com>
2016-09-21 11:43:49 -0400

    Met à jour le lien de la carte

--- includes/pages/membres.php
+++ includes/pages/membres.php
@@ -7,8 +7,8 @@
 </p>
 
 <iframe width="100%" height="300px" style="margin: 1em 0 0 0;" frameBorder="0"
-src="https://framacarte.org/en/map/untitled-map_5083?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe><p><a
-href="https://framacarte.org/en/map/untitled-map_5083"><?php echo _('En plein écran'); ?></a></p>
+src="https://framacarte.org/en/map/rezomen-sans-frontieres_5083?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe><p><a
+href="https://framacarte.org/en/map/rezomen-sans-frontieres_5083"><?php echo _('En plein écran'); ?></a></p>
 
 <?php
   $promo_array = array();
